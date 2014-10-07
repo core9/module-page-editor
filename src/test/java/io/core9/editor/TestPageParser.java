7c7c960e65d600b3a7060982a1a465543dc65263
jessec <robertjessecooke@gmail.com>
2014-10-07 21:06:34 +0200

    Adding first test

---
+++ src/test/java/io/core9/editor/TestPageParser.java
@@ -0,0 +1,14 @@
+package io.core9.editor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPageParser {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
