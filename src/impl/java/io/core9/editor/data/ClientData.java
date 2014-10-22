3de341e8af4801fdd27b9b0bf42f890f239e4faa
jessec <robertjessecooke@gmail.com>
2014-10-22 15:45:40 +0200

    cc

--- src/impl/java/io/core9/editor/data/ClientData.java
+++ src/impl/java/io/core9/editor/data/ClientData.java
@@ -5,41 +5,45 @@ import io.core9.editor.ClientRepositoryImpl;
 
 public class ClientData {
 
-	
-	
-	
+
+
+
 	public static ClientRepository getRepository(){
-		
+
 		ClientRepository clientRepository = new ClientRepositoryImpl();
-		
+
 		clientRepository.addDomain("www.easydrain.nl", "easydrain");
 		clientRepository.addDomain("easydrain.docker.trimm.net", "easydrain");
+		clientRepository.addDomain("easydrain.editor.core9.io", "easydrain");
 		clientRepository.addDomain("easydrain.localhost", "easydrain");
-		
+
 		clientRepository.addPage("www.easydrain.nl", "/nl/");
 		clientRepository.addPage("easydrain.docker.trimm.net", "/nl/");
+		clientRepository.addPage("easydrain.editor.core9.io", "/nl/");
 		clientRepository.addPage("easydrain.localhost", "/nl/");
-		
-		
+
+
 		clientRepository.addDomain("www.kennispark.nl", "kennispark");
 		clientRepository.addDomain("kennispark.editor.docker.trimm.net", "kennispark");
+		clientRepository.addDomain("kennispark.editor.core9.io", "kennispark");
 		clientRepository.addDomain("kennispark.localhost", "kennispark");
 
-		
+
 		clientRepository.addPage("www.kennispark.nl", "/jaarplan/");
 		clientRepository.addPage("kennispark.editor.docker.trimm.net", "/jaarplan/");
+		clientRepository.addPage("kennispark.editor.core9.io", "/jaarplan/");
 		clientRepository.addPage("kennispark.localhost", "/jaarplan/");
-		
-		
+
+
 		clientRepository.addSiteRepository("kennispark", "https://github.com/jessec/site-kennispark.git");
 		clientRepository.addBlockRepository("kennispark", "https://github.com/jessec/block-video.git");
-		
+
 		clientRepository.addSiteRepository("easydrain", "https://github.com/jessec/site-core9.git");
 		clientRepository.addBlockRepository("easydrain", "https://github.com/jessec/block-video.git");
-		
-		
+
+
 		return clientRepository;
 	}
-	
-	
+
+
 }
