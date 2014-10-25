527b4364943551547f1c99b6c20ffc3c8c9efa3d
jessec <robertjessecooke@gmail.com>
2014-10-25 18:50:37 +0200

    cc

--- src/test/java/io/core9/editor/TestPageData.java
+++ src/test/java/io/core9/editor/TestPageData.java
@@ -1,5 +1,51 @@
 package io.core9.editor;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
 public class TestPageData {
+
+	private static final String pathPrefix = "data/test-editor";
+	private AssetsManager assetsManager;
+	private EditorRequest request;
+	private String clientId = "9a8eccd84f9c40c791281139a87da7b645f25fab";
+	private ClientRepositoryImpl clientRepository;
+
+	@Test
+	public void testCreateClientDirectory() {
+		setupWorkingDirectory();
+		setUpRequest();
+		assetsManager.setRequest(request);
+		assertFalse(assetsManager.checkClientDirectory());
+		assetsManager.createClientDirectory();
+		assertTrue(assetsManager.checkClientDirectory());
+	}
+
+	private void setupWorkingDirectory() {
+		assetsManager = new AssetsManagerImpl(pathPrefix);
+		assetsManager.deleteWorkingDirectory();
+		assertFalse(assetsManager.checkWorkingDirectory());
+		assetsManager.createWorkingDirectory();
+		assertTrue(assetsManager.checkWorkingDirectory());
+	}
+
+	private void setUpRequest() {
+		clientRepository = new ClientRepositoryImpl();
+		clientRepository.addDomain("www.easydrain.nl", "easydrain");
+		clientRepository.addDomain("localhost", "easydrain");
+		request = new RequestImpl();
+		request.setClientRepository(clientRepository);
+		request.setAbsoluteUrl("http://localhost:8080/easydrain");
+	}
+
+	@AfterClass
+	public static void cleanup() {
+		AssetsManager assetsManager = new AssetsManagerImpl(pathPrefix);
+		assetsManager.deleteWorkingDirectory();
+		assertFalse(assetsManager.checkWorkingDirectory());
+	}
 
 }
