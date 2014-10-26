e4cd3da6ad92195a00f028beddbbe035667e291f
jessec <robertjessecooke@gmail.com>
2014-10-26 11:45:51 +0100

    cc

--- src/impl/java/io/core9/editor/PageDataParser.java
+++ src/impl/java/io/core9/editor/PageDataParser.java
@@ -1,5 +1,28 @@
 package io.core9.editor;
 
+import java.util.List;
+
 public interface PageDataParser {
+
+	List<Block> getBlocks();
+
+	void switchBlocks(int i, int j);
+
+	Block getBlock(int i);
+
+	String getOriginalFile();
+
+	void replaceBlock(int i, BlockData blockData);
+
+	String getPage();
+
+	void appendBlock(BlockData blockData);
+
+	void insertBlock(int i, BlockData blockData);
+
+	void deleteBlock(int i);
+
+	void deleteAllBlocks();
+
 
 }
--- src/impl/java/io/core9/editor/PageDataParserImpl.java
+++ src/impl/java/io/core9/editor/PageDataParserImpl.java
@@ -1,6 +1,7 @@
 package io.core9.editor;
 
 import java.io.File;
+import java.util.List;
 
 public class PageDataParserImpl implements PageDataParser {
 
@@ -8,6 +9,66 @@ public class PageDataParserImpl implements PageDataParser {
 
 	public PageDataParserImpl(File page) {
 		this.pageFile = page;
+	}
+
+	@Override
+	public List<Block> getBlocks() {
+
+		return null;
+	}
+
+	@Override
+	public void switchBlocks(int i, int j) {
+
+
+	}
+
+	@Override
+	public Block getBlock(int i) {
+
+		return null;
+	}
+
+	@Override
+	public String getOriginalFile() {
+
+		return null;
+	}
+
+	@Override
+	public void replaceBlock(int i, BlockData blockData) {
+
+
+	}
+
+	@Override
+	public String getPage() {
+
+		return null;
+	}
+
+	@Override
+	public void appendBlock(BlockData blockData) {
+
+
+	}
+
+	@Override
+	public void insertBlock(int i, BlockData blockData) {
+
+
+	}
+
+	@Override
+	public void deleteBlock(int i) {
+
+
+	}
+
+	@Override
+	public void deleteAllBlocks() {
+
+
 	}
 
 }
