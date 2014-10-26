cdb1ee27e80a06080b39a36e27738cb515117628
jessec <robertjessecooke@gmail.com>
2014-10-26 11:39:56 +0100

    cc

--- src/impl/java/io/core9/server/BlockUpdateToolImpl.java
+++ src/impl/java/io/core9/server/BlockUpdateToolImpl.java
@@ -34,9 +34,6 @@ public class BlockUpdateToolImpl implements BlockTool {
 	private String pathPrefix;
 	private AssetsManager assetsManager;
 	private RequestImpl request;
-	//private String httpsSiteRepositoryUrl = "https://github.com/jessec/site-kennispark.git";
-	//private String httpsBlockRepositoryUrl = "https://github.com/jessec/block-video.git";
-
 
 	@Override
 	public void setData(String prefix, JSONObject data) {
@@ -46,117 +43,79 @@ public class BlockUpdateToolImpl implements BlockTool {
 	}
 
 	private void processData() {
-
 		request = new RequestImpl();
 		request.setClientRepository(ClientData.getRepository());
-
 		JSONObject meta = (JSONObject) data.get("meta");
-
 		JSONObject editorData = (JSONObject) data.get("editor");
-
 		String absoluteUrl = meta.getAsString("absolute-url");
-
 		request.setAbsoluteUrl(absoluteUrl);
-
-
 		assetsManager = new AssetsManagerImpl(pathPrefix, request);
-		// assetsManager.deleteWorkingDirectory();
 		if (!assetsManager.checkWorkingDirectory()) {
 			assetsManager.createWorkingDirectory();
 		}
-
-
-
-
 		File siteConfig = new File(assetsManager.getSiteConfigFile());
 		if (!siteConfig.exists()) {
-
-			//FIXME same code in block command
+			// FIXME same code in block command
 			assetsManager.deleteClientDirectory();
 			assetsManager.createClientDirectory();
 			String clientId = assetsManager.getClientId();
-
 			ClientRepository repository = ClientData.getRepository();
 			String siteRepoUrl = repository.getSiteRepository(clientId);
-
 			assetsManager.clonePublicSiteFromGit(siteRepoUrl);
 			JSONObject config = assetsManager.getSiteConfig();
 			System.out.println(config);
-
-
 			try {
-
 				List<String> blockRepos = repository.getBlockRepository(clientId);
-				for(String repo : blockRepos){
+				for (String repo : blockRepos) {
 					assetsManager.cloneBlocksFromGit(repo);
 				}
-
-
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
-
-		} else {
-
 		}
-
 		System.out.println(assetsManager.getPageTemplate());
-
 		if (assetsManager.checkIfPageTemplateExists()) {
 			String pageTemplate = assetsManager.getPageTemplate();
 			System.out.println("");
-
 			File testPage = new File(pageTemplate);
 			if (testPage.exists()) {
 				parser = new PageParserImpl(testPage, blockContainer, blockClassName);
-
-
 				Block block = new BlockImpl();
-
-				if(meta.getAsString("state").equals("delete")){
+				if (meta.getAsString("state").equals("delete")) {
 					parser.deleteBlock(Integer.parseInt((String) meta.get("block")));
 				}
-
-				if(meta.getAsString("state").equals("insertbefore")){
+				if (meta.getAsString("state").equals("insertbefore")) {
 					String blockTemplate = meta.getAsString("template");
-					Element elem = parseSoyTemplateToElement(assetsManager.getClientId(), blockTemplate , editorData);
+					Element elem = parseSoyTemplateToElement(assetsManager.getClientId(), blockTemplate, editorData);
 					block.addElement(elem);
 					int pos = 0;
 					int selectedPos = Integer.parseInt((String) meta.get("block"));
-					if(selectedPos > 1){
+					if (selectedPos > 1) {
 						pos = selectedPos - 1;
 					}
 					parser.insertBlock(pos, block);
 				}
-
-				if(meta.getAsString("state").equals("insertafter")){
+				if (meta.getAsString("state").equals("insertafter")) {
 					String blockTemplate = meta.getAsString("template");
-					Element elem = parseSoyTemplateToElement(assetsManager.getClientId(), blockTemplate , editorData);
+					Element elem = parseSoyTemplateToElement(assetsManager.getClientId(), blockTemplate, editorData);
 					block.addElement(elem);
 					int selectedPos = Integer.parseInt((String) meta.get("block"));
 					int pos = selectedPos + 1;
 					parser.insertBlock(pos, block);
 				}
 
-				if(meta.getAsString("state").equals("edit")){
+				if (meta.getAsString("state").equals("edit")) {
 					String blockTemplate = meta.getAsString("template");
 					blockTemplate = blockTemplate.substring(11);
 
-					Element elem = parseSoyTemplateToElement(assetsManager.getClientId() + "/", blockTemplate , editorData);
+					Element elem = parseSoyTemplateToElement(assetsManager.getClientId() + "/", blockTemplate, editorData);
 					block.addElement(elem);
-					parser.replaceBlock(Integer.parseInt((String) meta.get("block")), block );
+					parser.replaceBlock(Integer.parseInt((String) meta.get("block")), block);
 				}
-
-
-
-
-
 				String htmlTemplate = parser.getPage();
-
-				if(!htmlTemplate.isEmpty()){
+				if (!htmlTemplate.isEmpty()) {
 					assetsManager.saveBlockData(meta, editorData);
 				}
-
 				System.out.println(htmlTemplate);
 				Document document = Jsoup.parse(htmlTemplate);
 				assetsManager.writePageCache(document.toString());
@@ -165,38 +124,19 @@ public class BlockUpdateToolImpl implements BlockTool {
 	}
 
 	private Element parseSoyTemplateToElement(String clientId, String blockTemplate, JSONObject data) {
-		 // Bundle the Soy files for your project into a SoyFileSet.
-
 		String[] tpl = blockTemplate.split("/");
-
 		String name = tpl[tpl.length - 1];
 		String[] nm = name.split("\\.");
 		String x = nm[0];
-
 		String soyNameSpace = x.replace("-", ".");
-
 		String blockDirectory = assetsManager.getBlockDirectory();
-
 		String soyTemplate = blockDirectory + blockTemplate;
-
-	    SoyFileSet sfs = new SoyFileSet.Builder().add(new File(soyTemplate)).build();
-
-	    // Compile the template into a SoyTofu object.
-	    // SoyTofu's newRenderer method returns an object that can render any template in the file set.
-	    SoyTofu tofu = sfs.compileToTofu();
-
-
-	    SoyMapData soyData = (SoyMapData) JsonSoyUtils.JsonToSoy(data.toJSONString());
-
-	    String blockHtml = tofu.newRenderer(soyNameSpace).setData(soyData).render();
-
-
-
-	    System.out.println(blockHtml);
-
-
-	    Element tag = Jsoup.parse(blockHtml, "", Parser.xmlParser());
-
+		SoyFileSet sfs = new SoyFileSet.Builder().add(new File(soyTemplate)).build();
+		SoyTofu tofu = sfs.compileToTofu();
+		SoyMapData soyData = (SoyMapData) JsonSoyUtils.JsonToSoy(data.toJSONString());
+		String blockHtml = tofu.newRenderer(soyNameSpace).setData(soyData).render();
+		System.out.println(blockHtml);
+		Element tag = Jsoup.parse(blockHtml, "", Parser.xmlParser());
 		return tag;
 	}
 
