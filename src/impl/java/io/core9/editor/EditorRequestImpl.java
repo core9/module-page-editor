012145231b96ae9af56545831133e9f6da98b073
jessec <robertjessecooke@gmail.com>
2014-10-27 16:43:01 +0100

    cc

--- src/impl/java/io/core9/editor/EditorRequestImpl.java
+++ src/impl/java/io/core9/editor/EditorRequestImpl.java
@@ -54,14 +54,14 @@ public class EditorRequestImpl implements EditorRequest {
 	}
 
 	private static Map<String, String> splitQuery(URL url) throws UnsupportedEncodingException {
-	    Map<String, String> query_pairs = new LinkedHashMap<String, String>();
-	    String query = url.getQuery();
-	    String[] pairs = query.split("&");
-	    for (String pair : pairs) {
-	        int idx = pair.indexOf("=");
-	        query_pairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
-	    }
-	    return query_pairs;
+		Map<String, String> query_pairs = new LinkedHashMap<String, String>();
+		String query = url.getQuery();
+		String[] pairs = query.split("&");
+		for (String pair : pairs) {
+			int idx = pair.indexOf("=");
+			query_pairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
+		}
+		return query_pairs;
 	}
 
 	@Override
@@ -71,13 +71,11 @@ public class EditorRequestImpl implements EditorRequest {
 			String urlStr = urlObject.toString();
 			url = new URL(urlStr);
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		try {
 			params = splitQuery(url);
 		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return params;
--- src/impl/java/io/core9/server/LoadClientSiteTemplatesAndKeepData.java
+++ src/impl/java/io/core9/server/LoadClientSiteTemplatesAndKeepData.java
@@ -2,8 +2,10 @@ package io.core9.server;
 
 import io.core9.editor.AssetsManager;
 import io.core9.editor.AssetsManagerImpl;
+import io.core9.editor.ClientRepository;
 import io.core9.editor.EditorRequest;
 import io.core9.editor.EditorRequestImpl;
+import io.core9.editor.data.ClientData;
 import net.minidev.json.JSONObject;
 
 public class LoadClientSiteTemplatesAndKeepData implements BlockTool {
@@ -17,6 +19,9 @@ public class LoadClientSiteTemplatesAndKeepData implements BlockTool {
 	public void setData(String prefix, JSONObject data) {
 
 		EditorRequest request = new EditorRequestImpl();
+		String absoluteUrl = null;
+		request.setAbsoluteUrl(absoluteUrl);
+		request.setClientRepository(ClientData.getRepository());
 		AssetsManager assetsManager = new AssetsManagerImpl("data/temp-editor", request);
 
 	}
