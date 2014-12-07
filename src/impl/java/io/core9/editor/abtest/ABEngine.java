576a594b3cde095009afbf9d5a652a6f311a21a9
jessec <robertjessecooke@gmail.com>
2014-12-07 17:12:44 +0100

    cc

--- src/impl/java/io/core9/editor/abtest/ABEngine.java
+++ src/impl/java/io/core9/editor/abtest/ABEngine.java
@@ -1,5 +1,7 @@
 package io.core9.editor.abtest;
 
+import java.util.UUID;
+
 import io.core9.module.auth.AuthenticationPlugin;
 import io.core9.module.auth.User;
 import io.core9.plugin.server.Cookie;
@@ -8,32 +10,37 @@ import io.core9.server.undertow.CookieImpl;
 
 public class ABEngine {
 
-	private static final String ABCOOKIE = "abtest";
+	private static final String CORE9SESSIONID = "CORE9SESSIONID";
+	private static final String ABCOOKIE = "CORE9DAT";
+
 
 	public static String getTestVars(AuthenticationPlugin auth, Request req) {
-		String test = null;
+		String abSessionId = getSessionID(auth, req);
+
+		return abSessionId;
+	}
+
+	public static String getSessionID(AuthenticationPlugin auth, Request req) {
+		String abSessionId = null;
+		String uuid = UUID.randomUUID().toString();
 		Cookie abCookie = req.getCookie(ABCOOKIE);
 		if(abCookie != null){
-			test = abCookie.getValue();
+			abSessionId = abCookie.getValue();
 		}
-
-
-		if(test == null){
-			Cookie ckie = req.getCookie("CORE9SESSIONID");
+		if(abSessionId == null){
 			User user = null;
-			if(ckie == null){
+			if(req.getCookie(CORE9SESSIONID) == null){
 				user = auth.getUser(req);
-			}else{
-				user = auth.getUser(req, ckie);
+			}else if(req.getCookie(CORE9SESSIONID) != null){
+				user = auth.getUser(req, req.getCookie(CORE9SESSIONID));
 			}
-
-			test = (String) user.getSession().getAttribute(ABCOOKIE);
-			if(test == null){
-				user.getSession().setAttribute(ABCOOKIE, "aaa");
-				req.getResponse().addCookie(new CookieImpl(new io.undertow.server.handlers.CookieImpl(ABCOOKIE, "aaa")));
+			abSessionId = (String) user.getSession().getAttribute(ABCOOKIE);
+			if(abSessionId == null){
+				user.getSession().setAttribute(ABCOOKIE, uuid);
+				req.getResponse().addCookie(new CookieImpl(new io.undertow.server.handlers.CookieImpl(ABCOOKIE, uuid).setPath("/")));
 			}
 		}
-		return test;
+		return abSessionId;
 	}
 
 
